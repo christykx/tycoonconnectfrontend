f83a15b290216828aafe487d8eda41a569fa474f
christykx <christykx2000@gmail.com>
2023-04-10 13:00:38 +0530

    last commit

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,8 @@ import axios from "axios";
 
 export const makeRequest = axios.create({
              
-    baseURL:"http://localhost:3001",
+    // baseURL:"http://localhost:3001",
+    baseURL:"https://tycoonconnect.online/",
     // "https://tycoonconnectapi.onrender.com",
     withCredentials:true
 })
