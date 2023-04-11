4b9b2587fab2b2db499264ed475511d4cf41d84c
christykx <christykx2000@gmail.com>
2023-04-11 12:41:52 +0530

    test2345

--- src/axios.js
+++ src/axios.js
@@ -2,12 +2,14 @@ import axios from "axios";
 
 
 // const token = localStorage.getItem('user')
-const token = localStorage.getItem('user1')
-console.log(token," from axios instence")
+// const token = localStorage.getItem('user1')
+
+const { currentUser } = useContext(AuthContext)
+console.log(currentUser," from axios instence")
 export const makeRequest = axios.create({
     baseURL: "https://tycoonconnect.online",
     headers: {
-        'Authorization': `Bearer ${token}`,
+        'Authorization': `Bearer ${currentUser?.accessToken}`,
         'Content-Type': 'application/json'
     },
     withCredentials: true
