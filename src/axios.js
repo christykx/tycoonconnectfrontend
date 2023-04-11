4c9ad3515b2f00869ac935529a1acf3cab3a0e91
christykx <christykx2000@gmail.com>
2023-04-11 12:55:49 +0530

    let342

--- src/axios.js
+++ src/axios.js
@@ -1,13 +1,14 @@
 import axios from "axios";
-import { AuthContext } from "./authContext/AuthContext";
-import { useContext } from "react";
+// import { AuthContext } from "./authContext/AuthContext";
+// import { useContext } from "react";
 
 
 // const token = localStorage.getItem('user')
-// const token = localStorage.getItem('user1')
+const token =  localStorage.getItem("user1")
 
-const { currentUser } = useContext(AuthContext)
-console.log(currentUser," from axios instence")
+// const { currentUser } = useContext(AuthContext)
+console.log(token," from axios instence")
+
 export const makeRequest = axios.create({
     baseURL: "https://tycoonconnect.online",
     headers: {
