8916e19ac89d190f26948b151b9cabdf60653be9
christykx <christykx2000@gmail.com>
2023-04-11 13:01:29 +0530

    test6576

--- src/axios.js
+++ src/axios.js
@@ -12,7 +12,7 @@ console.log(token," from axios instence")
 export const makeRequest = axios.create({
     baseURL: "https://tycoonconnect.online",
     headers: {
-        'Authorization': `Bearer ${currentUser?.accessToken}`,
+        'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json'
     },
     withCredentials: true
